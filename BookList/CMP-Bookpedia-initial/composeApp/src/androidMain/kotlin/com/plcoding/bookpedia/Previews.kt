472a696a5a59dd478afc7587b59a8f326e8f8726
Gor Sayamyan <gor_s@trebel.io>
2026-02-12 23:28:18 +0400

    Refactor: Move preview data out of state

--- BookList/CMP-Bookpedia-initial/composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
+++ BookList/CMP-Bookpedia-initial/composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
@@ -10,7 +10,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.plcoding.bookpedia.book.domain.Book
 import com.plcoding.bookpedia.book.presentation.book_list.BookListScreen
 import com.plcoding.bookpedia.book.presentation.book_list.BookListState
-import com.plcoding.bookpedia.book.presentation.book_list.books
 import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
 
 @Preview
@@ -31,6 +30,21 @@ private fun BookSearchBarPreview() {
     }
 }
 
+private val books = (1..100).map {
+    Book(
+        id = it.toString(),
+        title = "Book $it",
+        imageUrl = "https://test.com",
+        authors = listOf("Phillip Lackner"),
+        description = "Description $it",
+        languages = emptyList(),
+        firstPublishYear = null,
+        averageRating = 4.67854,
+        ratingCount = 5,
+        numPages = 100,
+        numEditions = 3
+    )
+}
 
 @Preview
 @Composable
